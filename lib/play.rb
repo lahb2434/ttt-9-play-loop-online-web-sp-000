472231182519648929763171d88848f3afd0a229
Chris Houghton <chrishoughton2434@gmail.com>
2020-08-12 00:59:45 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -37,7 +37,7 @@ end
 
 # Define your play method below
 def play(board)
- until board.each == "X" || board.each == "O"
+ until board.include?("X" || "O")
    turn(board)
    display_board(board)
   end
