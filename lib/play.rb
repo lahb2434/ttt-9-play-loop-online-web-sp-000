ae5bc57d0e295acdd59f761b860d06fa109cfdd3
Chris Houghton <chrishoughton2434@gmail.com>
2020-08-12 01:16:51 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -37,7 +37,7 @@ end
 
 # Define your play method below
 def play(board)
-  loop do board.each {|i| i != "X" || i != "O"}
+  while board.each {|i| i != " "}
      turn(board)
   end
    puts "winnnnner"
